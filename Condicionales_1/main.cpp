fda9088c9f33f6d67c91070502d507dd489dc5e4
Miguel Aceituno Rojo <Miguel Aceituno Rojo@DESKTOP-64UCGFH>
2023-12-09 09:19:20 -0500

    condicionales

---
+++ Condicionales_1/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y;
+    cout<<"Ingrese el valor de x"<<endl;
+    cin>>x;
+    cout<<"Ingrese el valor de y"<<endl;
+    cin>>y;
+
+    if(x<y)
+    {
+        //se ejecuta si la expresion es verdadera
+        cout<<"x es menor que y"<<endl;
+    }
+    else
+    {
+        //se ejecuta si la expresion es falso
+        cout<<"y es menor que x"<<endl;
+    }
+    return 0;
+}
---
+++ Condicionales_2/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y;
+    cout<<"Ingrese el valor de x"<<endl;
+    cin>>x;
+    cout<<"Ingrese el valor de y"<<endl;
+    cin>>y;
+
+    if(x<y)
+    {
+        //se ejecuta si la expresion if(x<y) es verdadera
+        cout<<"x es menor que y"<<endl;
+    }
+    else
+    {
+        //se ejecuta si la expresion if(x<y) es falso
+
+        if(x>y)
+        {
+            //se ejecuta si la expresion if(x>y) es verdadero
+            cout<<"x es mayor que y"<<endl;
+        }
+        else
+        {
+            //se ejecuta si la expresion if(x>y) es false
+            cout<<"x es igual que y"<<endl;
+        }
+
+
+    }
+    return 0;
+}
